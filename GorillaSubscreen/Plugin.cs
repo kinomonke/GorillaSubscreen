329f54c2448478c8df3b1be8972e99f4d49c8664
kino <kinogtag@outlook.com>
2025-12-07 18:01:44 +0000

    1.0.0

---
+++ GorillaSubscreen/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using GorillaSubscreen.Tools;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GorillaSubscreen
+{
+
+    [BepInPlugin(Constants.GUID, Constants.NAME, Constants.VERS)]
+    public class Plugin : BaseUnityPlugin
+    {
+        GameObject _GSPrefab;
+        void Awake() => GorillaTagger.OnPlayerSpawned(async() => await SetupBoard());
+
+        async Task SetupBoard()
+        {
+            try
+            {
+                _GSPrefab = await AssetLoader.LoadAsset<GameObject>("GorillaSubBoard");
+                if (_GSPrefab == null)
+                {
+                    Debug.LogError("[GS]: Failed to load AssetBundle.");
+                    return;
+                }
+                
+                var gsInstance = Instantiate(_GSPrefab);
+                gsInstance.SetActive(true);
+                gsInstance.transform.position = new Vector3(-62.8232f, 12.6908f, -83.8681f);
+                gsInstance.transform.rotation = Quaternion.Euler(0.886f, 95.4097f, 0f);
+            }
+            catch { Debug.LogError("[GS]: Error setting up board."); }
+        }
+    }
+    public class Constants
+    {
+        public const string GUID = "kino.gorillasubscreen",
+                            NAME = "GorillaSubscreen",
+                            VERS = "1.0.0";
+    }
+}
---
+++ GorillaSubscreen/Tools/AssetLoader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GorillaSubscreen.Tools
+{
+    //https://github.com/developer9998/GorillaHistoricalTeleporter/blob/main/GorillaHistoricalTeleporter/Tools/AssetLoader.cs (slightly modified)
+    public class AssetLoader
+    {
+        private static AssetBundle loadedBundle;
+        private static readonly Dictionary<string, Object> loadedAssets = new Dictionary<string, Object>();
+
+        private static Task bundleLoadTask;
+
+        public static async Task<T> LoadAsset<T>(string assetName) where T : Object
+        {
+            Object cached;
+            if (loadedAssets.TryGetValue(assetName, out cached) && cached is T)
+            {
+                return (T)cached;
+            }
+
+            if (loadedBundle == null)
+            {
+                if (bundleLoadTask == null)
+                {
+                    bundleLoadTask = LoadAssetBundle();
+                }
+                await bundleLoadTask;
+            }
+
+            var completionSource = new TaskCompletionSource<T>();
+
+            AssetBundleRequest request = loadedBundle.LoadAssetAsync<T>(assetName);
+            request.completed += _ =>
+            {
+                T result = request.asset as T;
+                completionSource.TrySetResult(result);
+            };
+
+            T loadedAsset = await completionSource.Task;
+
+            if (loadedAsset != null)
+            {
+                loadedAssets[assetName] = loadedAsset;
+            }
+
+            return loadedAsset;
+        }
+
+        private static async Task LoadAssetBundle()
+        {
+            var completionSource = new TaskCompletionSource<AssetBundle>();
+
+            Stream stream = typeof(AssetLoader).Assembly.GetManifestResourceStream("GorillaSubscreen.Content.gorillasub");
+            if (stream == null)
+            {
+                throw new FileNotFoundException("[GS]: Embedded asset bundle not found.");
+            }
+
+            AssetBundleCreateRequest request = AssetBundle.LoadFromStreamAsync(stream);
+            request.completed += _ =>
+            {
+                completionSource.TrySetResult(request.assetBundle);
+            };
+
+            loadedBundle = await completionSource.Task;
+        }
+    }
+}
